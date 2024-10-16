5a628ab5d890dcc092094f24d14a6c2926b441a0
Luis Gustavo <luisgsilva21@gmail.com>
2024-10-16 17:17:46 -0300

    feat: rotas de criacao de usuario e criacao de eventos (#2)

--- Backend/src/accounts/accounts.ts
+++ Backend/src/accounts/accounts.ts
@@ -16,10 +16,49 @@ export namespace AccountsHandler {
     password: string;
   };
 
+  /**
+   * Função para verificar se o email existe.
+   * @param pEmail email do usuário do tipo @type { string }
+   * @returns @type { boolean } se existe algum email cadastrado ou não.
+   */
+  async function verifyAccount(pEmail: string): Promise<boolean> {
+    let connection;
+
+    try {
+      connection = await OracleDB.getConnection(dbConfig);
+
+      const sql: string = `
+        SELECT TOKEN FROM ACCOUNTS WHERE email = :email
+      `;
+
+      const result = (await connection.execute(sql, [pEmail])).rows as [
+        { TOKEN: string }
+      ];
+
+      if (result && result.length > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      if (connection) {
+        try {
+          await connection.close();
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+
+    return false;
+  }
+
   async function login(email: string, password: string) {
     let connection = await OracleDB.getConnection(dbConfig);
 
-    const result = await connection.execute(
+    await connection.execute(
       `
             SELECT * 
             FROM ACCOUNTS 
@@ -28,9 +67,7 @@ export namespace AccountsHandler {
       [email, password]
     );
 
-    const linhas = result.rows;
-
-    console.dir(linhas, { depth: null });
+    await connection.commit();
 
     await connection.close();
 
@@ -54,11 +91,14 @@ export namespace AccountsHandler {
   };
 
   /**
-   * Buscar usuarios cadastrados
-   * @param req 
-   * @param res 
+   * Rota busca usuarios cadastrados
+   * @param req
+   * @param res
    */
-  export const getAllAccountsRoute = async (req: Request, res: Response): Promise<void> => {
+  export const getAllAccountsRoute: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<void> => {
     let connection;
 
     try {
@@ -73,10 +113,77 @@ export namespace AccountsHandler {
         FROM ACCOUNTS
       `;
 
-      const result: UserAccount[] | unknown = (await connection.execute(sql))
-        .rows;
+      const result = (await connection.execute(sql)).rows as UserAccount[];
 
-      res.status(200).send({ code: res.statusCode, msg: "Resultado da busca usuarios", usuarios: result });
+      res.status(200).send({
+        code: res.statusCode,
+        msg: "Resultado da busca usuarios",
+        usuarios: result,
+      });
+    } catch (err) {
+      console.error(err);
+    } finally {
+      if (connection) {
+        try {
+          await connection.close();
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+  };
+
+  /**
+   * Rota cadastrar usuario
+   * @param req
+   * @param res
+   */
+  export const putCreateAccountsRoute: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<void> => {
+    let connection;
+
+    try {
+      const pEmail = req.get("email");
+      const pPassword = req.get("password");
+      const pName = req.get("name");
+
+      if (pEmail && pPassword && pName) {
+        connection = await OracleDB.getConnection(dbConfig);
+
+        if (!(await verifyAccount(pEmail))) {
+          const sql: string = `
+          INSERT INTO ACCOUNTS (ID, EMAIL, PASSWORD, COMPLETE_NAME, TOKEN) VALUES
+          (
+              SEQ_ACCOUNTS.NEXTVAL,
+              :email,
+              :password,
+              :name,
+              dbms_random.string('x',64)
+          )
+        `;
+
+          await connection.execute(sql, [pEmail, pPassword, pName], {
+            autoCommit: true,
+          });
+
+          res.status(200).send({
+            code: res.statusCode,
+            msg: "Usuário criado com sucesso!",
+          });
+        } else {
+          res.status(200).send({
+            code: res.statusCode,
+            msg: "Já existe um usuário com essas credênciais",
+          });
+        }
+      } else {
+        res.status(400).send({
+          code: res.statusCode,
+          msg: "Requisição inválida - Parâmetros faltando.",
+        });
+      }
     } catch (err) {
       console.error(err);
     } finally {
--- Backend/src/events/events.ts
+++ Backend/src/events/events.ts
@@ -1,19 +1,93 @@
-export namespace EventHandler {
-    /**
-     * Tipo Event 
-     */
-    export type Event = {
-        id: number;
-        titulo: string;
-        descricao: string;
-        valorCota: number;
-        dataInicio: string;
-        dataFim: string;
-        data: string;
-        status: boolean;
-    }
+import { Request, RequestHandler, Response } from "express";
+import OracleDB from "oracledb";
+import { dbConfig } from "../dbConfig";
 
-    export async function getAllEvents() {
-        
+export namespace EventHandler {
+  /**
+   * Tipo Event
+   */
+  export type Event = {
+    id: number;
+    titulo: string;
+    descricao: string;
+    valorCota: number;
+    dataInicio: Date;
+    dataFim: Date;
+    data: Date;
+    status: boolean;
+  };
+
+  export async function getAllEvents() {}
+
+  export const postAddEventRoute: RequestHandler = async (
+    req: Request,
+    res: Response
+  ) => {
+    let connection;
+    try {
+      const { titulo, descricao, valorCota, dataInicio, dataFim, dataCriacao } =
+        req.body;
+
+      if (
+        titulo != "" &&
+        descricao != "" &&
+        dataInicio != "" &&
+        dataFim != "" &&
+        dataCriacao != "" &&
+        req.body
+      ) {
+        connection = await OracleDB.getConnection(dbConfig);
+
+        let cota = valorCota ? valorCota : 0;
+
+        const sql: string = `
+                    INSERT INTO EVENTS
+                    (
+                        id,
+                        titulo,
+                        descricao,
+                        valorCota,
+                        dataInicio,
+                        dataFim,
+                        dataCriacao,
+                        status
+                    ) VALUES (
+                        SEQ_EVENTS.NEXTVAL,
+                        :titulo,
+                        :descricao,
+                        :valorCota,
+                        TO_DATE(:dataInicio, 'YYYY-MM-DD'),
+                        TO_DATE(:dataFim, 'YYYY-MM-DD'),
+                        TO_DATE(:dataCriacao, 'YYYY-MM-DD'),
+                        0
+                    )
+                `;
+
+        await connection.execute(
+          sql,
+          [titulo, descricao, cota, dataInicio, dataFim, dataCriacao],
+          { autoCommit: true }
+        );
+
+        res
+          .status(200)
+          .send({ code: res.statusCode, msg: "Evento criado com sucesso" });
+      } else {
+        res.status(400).send({
+          code: res.statusCode,
+          msg: "Requisição inválida - Parâmetros faltando.",
+        });
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      if (connection) {
+        try {
+          await connection.close();
+        } catch (err) {
+          console.error(err);
+        }
+      }
     }
+  };
 }
