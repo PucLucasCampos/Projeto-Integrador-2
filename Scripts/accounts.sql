57ebc1748dbcdab42c28a799861a78df2dd70ea5
LUCAS DE CAMPOS RANZANI <lucas.cr16@puccampinas.edu.br>
2024-10-16 09:43:03 -0300

    oracle clound

--- Scripts/accounts.sql
+++ Scripts/accounts.sql
@@ -2,20 +2,34 @@ CREATE TABLE ACCOUNTS (
     ID INTEGER NOT NULL PRIMARY KEY,
     EMAIL VARCHAR2(100) NOT NULL UNIQUE,
     PASSWORD VARCHAR2(32) NOT NULL,
-    COMPLETE_NAME VARCHAR2(100) NOT NULL
+    COMPLETE_NAME VARCHAR2(100) NOT NULL,
+    TOKEN VARCHAR2(64) NOT NULL
 );
 
 CREATE SEQUENCE SEQ_ACCOUNTS START WITH 1 
 INCREMENT BY 1;
 
-INSERT INTO ACCOUNTS (ID, EMAIL, PASSWORD, COMPLETE_NAME) VALUES
+set serveroutput on
+begin
+  for i in 1 .. 5 loop
+    dbms_output.put_line('string(''x'',10)= ' || dbms_random.string('x',64));
+  end loop;
+end;
+
+INSERT INTO ACCOUNTS (ID, EMAIL, PASSWORD, COMPLETE_NAME, TOKEN) VALUES
 (
     SEQ_ACCOUNTS.NEXTVAL,
     'luis@puccampinas.edu.br',
     'luis',
-    'Luis Silva'
+    'Luis Silva',
+    dbms_random.string('x',64)
 );
 
 COMMIT;
 
 SELECT * FROM ACCOUNTS;
+
+DROP TABLE ACCOUNTS;
+
+SELECT TOKEN FROM ACCOUNTS WHERE email = 'luis@puccampinas.edu.br' AND password = 'luis';
+
