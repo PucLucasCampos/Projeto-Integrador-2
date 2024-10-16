9698c6b48a5c7d1fb105a2655184d3d4cf84b654
PucLucasCampos <lucas.cr16@puccampinas.edu.br>
2024-10-16 16:57:45 -0300

    Trabalhei na rota /getEvents, fiz a função de mostrar todos os eventos(sem pasar parametros nenhum)

--- Backend/src/events/events.ts
+++ Backend/src/events/events.ts
@@ -1,19 +1,47 @@
+import { Request, RequestHandler, Response } from "express";
+import OracleDB from "oracledb";
+import { dbConfig } from "../dbConfig";
 export namespace EventHandler {
-    /**
-     * Tipo Event 
-     */
-    export type Event = {
-        id: number;
-        titulo: string;
-        descricao: string;
-        valorCota: number;
-        dataInicio: string;
-        dataFim: string;
-        data: string;
-        status: boolean;
-    }
+   /**
+    * Tipo Event
+    */
+   export type Event = {
+      id: number;
+      titulo: string;
+      descricao: string;
+      valorCota: number;
+      dataInicio: string;
+      dataFim: string;
+      data: string;
+      status: boolean;
+   };
 
-    export async function getAllEvents() {
-        
-    }
+   export const getAllEvents = async (req: Request, res: Response): Promise<void> => {
+      let connection;
+
+      try {
+         connection = await OracleDB.getConnection(dbConfig);
+
+         const sql: string = `
+            SELECT * FROM EVENTS
+         `;
+
+         const result: Event[] | unknown = (await connection.execute(sql)).rows;
+         res.status(200).send({
+            code: res.statusCode,
+            msg: "Resultado da busca Eventos",
+            events: result,
+         });
+      } catch (err) {
+         console.log(err);
+      } finally {
+         if (connection) {
+            try {
+               await connection.close();
+            } catch (err) {
+               console.log(err);
+            }
+         }
+      }
+   };
 }
